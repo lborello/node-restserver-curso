60dafc4a28b41b985bab07470d105856cf41c803
Luis Borello <luis_borello@hotmail.com>
2019-11-20 12:03:37 -0300

    detectar el local

--- server/server.js
+++ server/server.js
@@ -13,7 +13,7 @@ app.use(bodyParser.json())
 
 
 app.get('/usuario', function(req, res) {
-    res.json('get Usuario LOCAL!!!');
+    res.json('get Usuario :) LOCALddddd!!!');
 });
 
 app.post('/usuario', function(req, res) {
